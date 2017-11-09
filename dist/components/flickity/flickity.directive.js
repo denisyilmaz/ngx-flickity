462a94e9050c9731f0f352cf97bc7c115f8eae72
Denis <mail@denisyilmaz.de>
2017-11-09 13:40:49 +0100

    back to start

--- dist/components/flickity/flickity.directive.js
+++ dist/components/flickity/flickity.directive.js
@@ -16,18 +16,155 @@ var FlickityDirective = /** @class */ (function () {
         this.el = el;
         this.appConfigService = appConfigService;
         this.config = {};
+        this.slideSelect = new core_1.EventEmitter();
+        this.cellStaticClick = new core_1.EventEmitter();
+        this.childrenUpdated = new core_1.EventEmitter();
+        this.appendElements = [];
+        this.childrenUpdateInterval = 300;
     }
     FlickityDirective.prototype.ngAfterContentInit = function () {
-        // this.init();
-        console.log('HI');
+        this.init();
     };
     FlickityDirective.prototype.ngOnDestroy = function () {
-        // this.destroy();
+        this.destroy();
+    };
+    FlickityDirective.prototype.init = function () {
+        var _this = this;
+        if (this.appConfigService.isPlatformServer) {
+            return;
+        }
+        var Flickity = require('flickity');
+        var config = this.config;
+        if (this.flkty) {
+            config['initialIndex'] = this.flkty.selectedIndex;
+            this.destroy();
+        }
+        this.flkty = new Flickity(this.el.nativeElement, config);
+        this.flkty.on('select', function () {
+            _this.slideSelect.emit(_this.selectedIndex);
+        });
+        this.flkty.on('staticClick', function (_event, _pointer, _cellElement, cellIndex) {
+            _this.cellStaticClick.emit(cellIndex);
+        });
+        this.updateElements();
+        setTimeout(function () { return _this.resize(); }, this.childrenUpdateInterval);
+    };
+    FlickityDirective.prototype.destroy = function () {
+        if (!this.flkty) {
+            return;
+        }
+        if (this.childrenUpdate) {
+            clearInterval(this.childrenUpdate);
+            this.childrenUpdate = undefined;
+        }
+        this.flkty.destroy();
+    };
+    FlickityDirective.prototype.resize = function () {
+        if (!this.flkty) {
+            return;
+        }
+        this.flkty.resize();
+    };
+    FlickityDirective.prototype.reposition = function () {
+        if (!this.flkty) {
+            return;
+        }
+        this.flkty.reposition();
+    };
+    FlickityDirective.prototype.previous = function () {
+        if (!this.flkty) {
+            return;
+        }
+        this.flkty.previous();
+    };
+    FlickityDirective.prototype.previousAvailable = function () {
+        if (this.selectedIndex === undefined) {
+            return false;
+        }
+        return this.selectedIndex > 0;
+    };
+    FlickityDirective.prototype.next = function () {
+        if (!this.flkty) {
+            return;
+        }
+        this.flkty.next();
+    };
+    FlickityDirective.prototype.nextAvailable = function () {
+        if (this.selectedIndex === undefined || !this.slides) {
+            return false;
+        }
+        return this.selectedIndex < this.slides - 1;
+    };
+    Object.defineProperty(FlickityDirective.prototype, "selectedIndex", {
+        get: function () {
+            if (!this.flkty) {
+                return;
+            }
+            return this.flkty.selectedIndex;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(FlickityDirective.prototype, "cells", {
+        get: function () {
+            if (!this.flkty) {
+                return;
+            }
+            return this.flkty.cells.length;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(FlickityDirective.prototype, "slides", {
+        get: function () {
+            if (!this.flkty) {
+                return;
+            }
+            return this.flkty['slides'].length;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    FlickityDirective.prototype.append = function (el) {
+        this.appendElements.push(el);
+    };
+    FlickityDirective.prototype.prepend = function (el) {
+        this.flkty.prepend(el);
+    };
+    FlickityDirective.prototype.updateElements = function () {
+        var _this = this;
+        if (!this.flkty || this.appendElements.length == 0) {
+            return;
+        }
+        this.appendElements.forEach(function (el) {
+            _this.flkty.append(el);
+        });
+        this.appendElements = [];
+        this.resize();
+        this.childrenUpdated.emit();
+        this.childrenUpdate = setTimeout(this.updateElements(), this.childrenUpdateInterval);
+    };
+    FlickityDirective.prototype.select = function (index, isWrapped, isInstant) {
+        if (isWrapped === void 0) { isWrapped = true; }
+        if (isInstant === void 0) { isInstant = false; }
+        this.flkty.select(index, isWrapped, isInstant);
     };
     __decorate([
         core_1.Input('flickity'),
         __metadata("design:type", Object)
     ], FlickityDirective.prototype, "config", void 0);
+    __decorate([
+        core_1.Output(),
+        __metadata("design:type", Object)
+    ], FlickityDirective.prototype, "slideSelect", void 0);
+    __decorate([
+        core_1.Output(),
+        __metadata("design:type", Object)
+    ], FlickityDirective.prototype, "cellStaticClick", void 0);
+    __decorate([
+        core_1.Output(),
+        __metadata("design:type", Object)
+    ], FlickityDirective.prototype, "childrenUpdated", void 0);
     FlickityDirective = __decorate([
         core_1.Directive({ selector: '[flickity]' }),
         __metadata("design:paramtypes", [core_1.ElementRef,
--- src/components/flickity/flickity.directive.ts
+++ src/components/flickity/flickity.directive.ts
@@ -1,7 +1,7 @@
 declare let require: any;
 
 import {
-  ElementRef, Directive, OnDestroy, Input, Output, EventEmitter, AfterContentInit
+  Directive, OnDestroy, Input, Output, EventEmitter, AfterContentInit, ElementRef
 } from '@angular/core';
 
 import { FlickityOptions } from "../../interfaces/flickity-options.interface";
@@ -11,157 +11,164 @@ import { AppConfigService } from '../../services/app-config.service';
 export class FlickityDirective implements AfterContentInit, OnDestroy {
 
   @Input('flickity') config: FlickityOptions = {};
+  @Output() slideSelect = new EventEmitter<number>();
+  @Output() cellStaticClick = new EventEmitter<number>();
+  @Output() childrenUpdated = new EventEmitter<void>();
 
+  private flkty: any;
+  private appendElements: HTMLElement[] = [];
+  private childrenUpdate;
+  private childrenUpdateInterval = 300;
+  
   constructor(private el: ElementRef,
               private appConfigService: AppConfigService) { }
 
   ngAfterContentInit(): void {
-    // this.init();
-    console.log('HI');
+    this.init();
   }
 
   ngOnDestroy(): void {
-    // this.destroy();
+    this.destroy();
   }
 
-  // init() {
-  //   if (this.appConfigService.isPlatformServer || !this.flickityElement) {
-  //     return;
-  //   }
+  init() {
+    if (this.appConfigService.isPlatformServer) {
+      return;
+    }
 
-  //   const Flickity = require('flickity');
+    const Flickity = require('flickity');
 
-  //   let config = this.config;
+    let config = this.config;
 
-  //   if (this.flkty) {
-  //     config['initialIndex'] = this.flkty.selectedIndex;
-  //     this.destroy();
-  //   }
+    if (this.flkty) {
+      config['initialIndex'] = this.flkty.selectedIndex;
+      this.destroy();
+    }
 
-  //   this.flkty = new Flickity(this.flickityElement, config);
+    this.flkty = new Flickity(this.el.nativeElement, config);
 
-  //   this.flkty.on('select', () => {
-  //     this.slideSelect.emit(this.selectedIndex);
-  //   });
+    this.flkty.on('select', () => {
+      this.slideSelect.emit(this.selectedIndex);
+    });
 
-  //   this.flkty.on('staticClick', (_event: any, _pointer: any, _cellElement: any, cellIndex: any) => {
-  //     this.cellStaticClick.emit(cellIndex);
-  //   });
+    this.flkty.on('staticClick', (_event: any, _pointer: any, _cellElement: any, cellIndex: any) => {
+      this.cellStaticClick.emit(cellIndex);
+    });
 
-  //   this.updateElements();
+    this.updateElements();
 
-  //   setTimeout(() => this.resize(), this.childrenUpdateInterval);
-  // }
+    setTimeout(() => this.resize(), this.childrenUpdateInterval);
+  }
 
-  // destroy() {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  destroy() {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   if (this.childrenUpdate) {
-  //     clearInterval(this.childrenUpdate);
-  //     this.childrenUpdate = undefined;
-  //   }
+    if (this.childrenUpdate) {
+      clearInterval(this.childrenUpdate);
+      this.childrenUpdate = undefined;
+    }
 
-  //   this.flkty.destroy();
-  // }
+    this.flkty.destroy();
+  }
 
-  // resize() {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  resize() {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   this.flkty.resize();
-  // }
+    this.flkty.resize();
+  }
 
-  // reposition() {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  reposition() {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   this.flkty.reposition();
-  // }
+    this.flkty.reposition();
+  }
 
-  // previous() {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  previous() {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   this.flkty.previous();
-  // }
+    this.flkty.previous();
+  }
 
-  // previousAvailable(): boolean {
-  //   if (this.selectedIndex === undefined) {
-  //     return false;
-  //   }
+  previousAvailable(): boolean {
+    if (this.selectedIndex === undefined) {
+      return false;
+    }
 
-  //   return this.selectedIndex > 0;
-  // }
+    return this.selectedIndex > 0;
+  }
 
-  // next() {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  next() {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   this.flkty.next();
-  // }
+    this.flkty.next();
+  }
 
-  // nextAvailable(): boolean {
-  //   if (this.selectedIndex === undefined || !this.slides) {
-  //     return false;
-  //   }
+  nextAvailable(): boolean {
+    if (this.selectedIndex === undefined || !this.slides) {
+      return false;
+    }
 
-  //   return this.selectedIndex < this.slides - 1;
-  // }
+    return this.selectedIndex < this.slides - 1;
+  }
 
-  // get selectedIndex(): number {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  get selectedIndex(): number {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   return this.flkty.selectedIndex;
-  // }
+    return this.flkty.selectedIndex;
+  }
 
-  // get cells(): number {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  get cells(): number {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   return this.flkty.cells.length;
-  // }
+    return this.flkty.cells.length;
+  }
 
-  // get slides(): number {
-  //   if (!this.flkty) {
-  //     return;
-  //   }
+  get slides(): number {
+    if (!this.flkty) {
+      return;
+    }
 
-  //   return this.flkty['slides'].length;
-  // }
+    return this.flkty['slides'].length;
+  }
 
-  // append(el: HTMLElement) {
-  //   this.appendElements.push(el);
-  // }
+  append(el: HTMLElement) {
+    this.appendElements.push(el);
+  }
 
-  // prepend(el: HTMLElement) {
-  //   this.flkty.prepend(el);
-  // }
+  prepend(el: HTMLElement) {
+    this.flkty.prepend(el);
+  }
 
-  // private updateElements() {
-  //   if (!this.flkty || this.appendElements.length == 0) {
-  //     return;
-  //   }
+  private updateElements() {
+    if (!this.flkty || this.appendElements.length == 0) {
+      return;
+    }
 
-  //   this.appendElements.forEach(el => {
-  //     this.flkty.append(el)
-  //   });
-  //   this.appendElements = [];
+    this.appendElements.forEach(el => {
+      this.flkty.append(el)
+    });
+    this.appendElements = [];
 
-  //   this.resize();
-  //   this.childrenUpdated.emit();
-  //   this.childrenUpdate = setTimeout(this.updateElements(), this.childrenUpdateInterval);
-  // }
+    this.resize();
+    this.childrenUpdated.emit();
+    this.childrenUpdate = setTimeout(this.updateElements(), this.childrenUpdateInterval);
+  }
 
-  // select(index: number, isWrapped = true, isInstant = false) {
-  //   this.flkty.select(index, isWrapped, isInstant);
-  // }
+  select(index: number, isWrapped = true, isInstant = false) {
+    this.flkty.select(index, isWrapped, isInstant);
+  }
 }
