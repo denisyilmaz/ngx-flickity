065f12388878bb85b823e8a5ba7b713f1a3ed3f4
Denis <mail@denisyilmaz.de>
2017-11-09 12:44:59 +0100

    new bild

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,6 @@
-import { FlickityDirective } from './components/flickity/flickity.directive';
-import { FlickityChildDirective } from './components/flickity-child/flickity-child.directive';
-import { FlickityModule } from './flickity.module';
-
 export { FlickityOptions } from './interfaces/flickity-options.interface';
 export { FlickityDirective } from './components/flickity/flickity.directive';
 export { FlickityChildDirective } from './components/flickity-child/flickity-child.directive';
 export { FlickityModule } from './flickity.module';
 
-export let FLICKITY_DIRECTIVES = [
-    FlickityDirective,
-    FlickityChildDirective
-];
+
